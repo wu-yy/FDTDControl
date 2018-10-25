73c1f07b94465bb87e0f066e81b91f7b5dd38377
蓝鲸123 <wuyongyu13@126.com>
2018-10-26 01:08:23 +0800

    Update MouseHookHelper.cs

--- FDTDControl/MouseHookHelper.cs
+++ FDTDControl/MouseHookHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -127,6 +127,23 @@ namespace FDTDControl
         [DllImport("user32.dll")]
         public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
+        #endregion
+
+        #region
+
+        [DllImport("user32.dll", EntryPoint = "keybd_event")]
+
+        public static extern void keybd_event(
+
+            byte bVk,    //虚拟键值
+
+            byte bScan,// 一般为0
+
+            int dwFlags,  //这里是整数类型  0 为按下，2为释放
+
+            int dwExtraInfo  //这里是整数类型 一般情况下设成为 0
+
+        );        
         #endregion
 
         #region 获取坐标钩子
